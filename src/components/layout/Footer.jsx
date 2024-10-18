8a334d13020ee25e37495537f39942fb3f86fd12
Cumstein <kamyabaghaz@gmail.com>
2024-10-19 00:27:57 +0330

    first commit

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,20 @@
+import { Typography } from "@mui/material";
+
+function Footer() {
+  return (
+    <footer>
+      <Typography
+        component="p"
+        variant="p"
+        bgcolor="#f7f7f7"
+        color="primary"
+        padding="10px"
+        textAlign="center"
+        mt={10}
+      >
+        پروژه ی وبلاگ با GraphQL | سلامت لاگ 2024
+      </Typography>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/graphql/queries.js
@@ -0,0 +1,101 @@
+import { gql } from "@apollo/client";
+
+const GET_BLOGS_INFO = gql`
+  query {
+    posts {
+      id
+      slug
+      stage
+      title
+      coverPhoto {
+        url
+      }
+      author {
+        ... on Author {
+          id
+          name
+          avatar {
+            url
+          }
+        }
+      }
+    }
+  }
+`;
+
+const GET_AUTHORS_INFO = gql`
+  query {
+    authors {
+      id
+      name
+      slug
+      avatar {
+        url
+      }
+    }
+  }
+`;
+
+const GET_AUTHOR_INFO = gql`
+  query getAuthorInfo($slug: String!) {
+    author(where: { slug: $slug }) {
+      avatar {
+        url
+      }
+      field
+      name
+      description {
+        html
+      }
+      posts {
+        coverPhoto {
+          url
+        }
+        id
+        slug
+        title
+      }
+    }
+  }
+`;
+
+const GET_POST_INFO = gql`
+  query getPost($slug: String!) {
+    post(where: { slug: $slug }) {
+      author {
+        ... on Author {
+          id
+          name
+          avatar {
+            url
+          }
+          field
+        }
+      }
+      title
+      coverPhoto {
+        url
+      }
+      content {
+        html
+      }
+    }
+  }
+`;
+
+const GET_POST_COMMENTS = gql`
+  query getPostComments($slug: String!) {
+    comments(where: { post: { slug: $slug } }) {
+      id
+      name
+      text
+    }
+  }
+`;
+export {
+  GET_BLOGS_INFO,
+  GET_AUTHORS_INFO,
+  GET_AUTHOR_INFO,
+  GET_POST_INFO,
+  GET_POST_COMMENTS,
+};
